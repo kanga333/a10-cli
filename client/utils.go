4829ca7f8ff84aff03611e7ff03f691432daf12a
kanga333 <e411z7t40w@gmail.com>
2017-04-18 22:33:27 +0900

    add a10 boolean type

---
+++ client/utils.go
@@ -0,0 +1,27 @@
+package client
+
+import "fmt"
+
+//A10Bool is a boolean value that converts between 0 and 1
+type a10Bool bool
+
+func (a a10Bool) MarshalJSON() ([]byte, error) {
+	if a {
+		return []byte("1"), nil
+	}
+	return []byte("0"), nil
+}
+
+func (a *a10Bool) UnmarshalJSON(data []byte) error {
+	asString := string(data)
+	switch asString {
+	case "0":
+		*a = false
+		return nil
+	case "1":
+		*a = true
+		return nil
+	default:
+		return fmt.Errorf("")
+	}
+}
---
+++ client/utils_test.go
@@ -0,0 +1,57 @@
+package client
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+const (
+	falseData   = `{"boolean":0}`
+	trueData    = `{"boolean":1}`
+	illegalData = `{"boolean":100}`
+)
+
+func TestA10BooL(t *testing.T) {
+	type asBoolean struct {
+		Boolean a10Bool `json:"boolean"`
+	}
+
+	var asTrue asBoolean
+	err := json.Unmarshal([]byte(trueData), &asTrue)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	if asTrue.Boolean != true {
+		t.Error("asTrue.Boolean should be true but", asTrue.Boolean)
+	}
+	trueByte, err := json.Marshal(&asTrue)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	if string(trueByte) != trueData {
+		t.Error("asTrue.Boolean should be true but", string(trueByte))
+	}
+
+	var asFalse asBoolean
+	err = json.Unmarshal([]byte(falseData), &asFalse)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	if asFalse.Boolean != false {
+		t.Error("asFalse.Boolean should be true but", asFalse.Boolean)
+	}
+	falseByte, err := json.Marshal(&asFalse)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	if string(falseByte) != falseData {
+		t.Error("asTrue.Boolean should be true but", string(falseByte))
+	}
+
+	var asNil asBoolean
+	err = json.Unmarshal([]byte(illegalData), &asNil)
+	if err == nil {
+		t.Error("should raise error")
+	}
+
+}
