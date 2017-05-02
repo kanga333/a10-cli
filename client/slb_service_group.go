da752527c33fea5c99b0ebe6e6271705bf571e10
kanga333 <e411z7t40w@gmail.com>
2017-05-02 23:11:43 +0900

    add ServiceGroupSearch

--- client/slb_service_group.go
+++ client/slb_service_group.go
@@ -2,18 +2,41 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 )
 
 const (
 	memberCreate = "slb.service_group.member.create"
 	memberDelete = "slb.service_group.member.delete"
+	sgSearch     = "slb.service_group.search"
 )
 
 //SGNameAndMember represents slb.service_group.member io object of A10.
 type SGNameAndMember struct {
 	Name   string `json:"name"`
 	Member Member `json:"member"`
+}
+
+//ServiceGroup represents slb.service_group object of A10.
+type ServiceGroup struct {
+	Name                       string          `json:"name"`
+	Protocol                   int             `json:"protocol"`
+	LbMethod                   int             `json:"lb_method"`
+	HealthMonitor              string          `json:"health_monitor"`
+	MinActiveMember            MinActiveMember `json:"min_active_member"`
+	BackupServerEventLogEnable int             `json:"backup_server_event_log_enable"`
+	ClientReset                int             `json:"client_reset"`
+	StatsData                  int             `json:"stats_data"`
+	ExtendedStats              int             `json:"extended_stats"`
+	MemberList                 []Member        `json:"member_list"`
+}
+
+//MinActiveMember represents slb.service_group.min_active_member object of A10.
+type MinActiveMember struct {
+	Status      int `json:"status"`
+	Number      int `json:"number"`
+	PrioritySet int `json:"priority_set"`
 }
 
 //Member represents slb.service_group.member member object of A10.
@@ -76,4 +99,46 @@ func (c *Client) ServiceGroupMemberDelete(m *SGNameAndMember) error {
 	defer resp.Body.Close()
 
 	return nil
+}
+
+//ServiceGroupSearch is a function to slb.service_group.search to a10.
+func (c *Client) ServiceGroupSearch(n string) (*ServiceGroup, error) {
+	log.Println("Start sg search.")
+
+	url, err := c.CreateSessionURL(sgSearch)
+	if err != nil {
+		log.Println("Error in creating session url.")
+		return nil, err
+	}
+
+	var name struct {
+		Neme string `json:"name"`
+	}
+	name.Neme = n
+
+	body, err := json.Marshal(name)
+	if err != nil {
+		log.Println("Error in creating server delete request.")
+		return nil, err
+	}
+
+	resp, err := c.postJSON(url, body)
+	if err != nil {
+		log.Println("Error in server create request.")
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var jsonBody struct {
+		ServiceGroup ServiceGroup `json:"service_group"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&jsonBody)
+	if err != nil {
+		log.Println("Error in parsing serverSearch request response.")
+		return nil, err
+	}
+	if &jsonBody == nil {
+		return nil, fmt.Errorf("Struct after JSON parsing is empty")
+	}
+
+	return &jsonBody.ServiceGroup, nil
 }
--- client/slb_service_group_test.go
+++ client/slb_service_group_test.go
@@ -35,6 +35,44 @@ const testSGMemberSubset = `
 }
 `
 
+const testServiceGroup = `
+{
+    "service_group": {
+        "name": "VBX2842DH"
+        ,"protocol": 2
+        ,"lb_method": 2
+        ,"health_monitor": "L8T0"
+        ,"min_active_member": {
+            "status": 0
+            ,"number": 53
+            ,"priority_set": 1
+        }
+        ,"backup_server_event_log_enable": 0
+        ,"client_reset": 0
+        ,"stats_data": 0
+        ,"extended_stats": 0
+        ,"member_list": [
+            {
+                "server": ""
+                ,"port": 51084
+                ,"template": ""
+                ,"priority": 12
+                ,"status": 0
+                ,"stats_data": 0
+            }
+            ,{
+                "server": "BT2DN60F"
+                ,"port": 34144
+                ,"template": "B"
+                ,"priority": 9
+                ,"status": 0
+                ,"stats_data": 0
+            }
+        ]
+    }
+}
+`
+
 func TestServiceGroupMemberCreate(t *testing.T) {
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		if req.URL.Path != "/services/rest/V2.1/" {
@@ -159,4 +197,66 @@ func TestServiceGroupMemberDelete(t *testing.T) {
 	if err != nil {
 		t.Fatalf("should not raise error: %v", err)
 	}
+}
+
+func TestServiceGroupSearch(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/services/rest/V2.1/" {
+			t.Error("request URL should be /services/rest/V2.1/ but :", req.URL.Path)
+		}
+		if req.Method != "POST" {
+			t.Error("request method should be POST but: ", req.Method)
+		}
+		query := req.URL.Query()
+		if strings.Join(query["method"], "") != "slb.service_group.search" {
+			t.Error("request QueryString should be method=slb.service_group.search but :", query["method"])
+		}
+
+		var jsonBody struct {
+			Neme string `json:"name"`
+		}
+		err := json.NewDecoder(req.Body).Decode(&jsonBody)
+		if err != nil {
+			t.Error("request body should be decoded as json", err)
+		}
+
+		if jsonBody.Neme != "VBX2842DH" {
+			t.Errorf("reqServer should be VBX2842DH but %v", jsonBody.Neme)
+		}
+
+		respJSON := testServiceGroup
+		res.Header()["Content-Type"] = []string{"application/json"}
+		fmt.Fprint(res, respJSON)
+	}))
+	defer ts.Close()
+
+	u, _ := url.Parse(ts.URL)
+	opts := Opts{
+		Username: "admin",
+		Password: "passwd",
+		Target:   u.Host,
+		Insecure: true,
+	}
+
+	client, err := NewClient(opts)
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	client.token = "FTNFPTD"
+	sg, err := client.ServiceGroupSearch("VBX2842DH")
+	if err != nil {
+		t.Fatalf("should not raise error: %v", err)
+	}
+
+	var expectServiceGroup struct {
+		ServiceGroup ServiceGroup `json:"service_group"`
+	}
+	err = json.Unmarshal([]byte(testServiceGroup), &expectServiceGroup)
+	if err != nil {
+		t.Error("testServerData should be decoded as json")
+	}
+	if reflect.DeepEqual(sg, expectServiceGroup.ServiceGroup) {
+		t.Errorf("reqServer should be %v but %v", expectServiceGroup.ServiceGroup, sg)
+	}
+
 }
