1b8893e003942a7f1ba89ccad7a42f835b4c55ad
kanga333 <e411z7t40w@gmail.com>
2017-05-05 09:04:08 +0900

    add ServerDefault

--- config/config.go
+++ config/config.go
@@ -8,6 +8,25 @@ import (
 	"github.com/kanga333/a10-cli/client"
 )
 
+var (
+	DefaultConfig = Config{
+		Server: client.Server{
+			GslbExternalAddress: "0.0.0.0",
+			Weight:              1,
+			HealthMonitor:       "(default)",
+			Status:              false,
+			ConnLimit:           8000000,
+			ConnLimitLog:        true,
+			ConnResume:          0,
+			StatsData:           true,
+			ExtendedStats:       false,
+			SlowStart:           false,
+			SpoofingCache:       false,
+			Template:            "default",
+		},
+	}
+)
+
 // Config is a structure that expresses the setting required by a10
 type Config struct {
 	A10    client.Opts   `json:"a10cli"`
