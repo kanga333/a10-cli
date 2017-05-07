986d0f10fc1ebdb8e6b2e734e6f46b4066d6798e
kanga333 <e411z7t40w@gmail.com>
2017-05-07 23:46:10 +0900

    add join

--- command/join.go
+++ command/join.go
@@ -1,8 +1,66 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/kanga333/a10-cli/client"
+	"github.com/kanga333/a10-cli/config"
+)
 
 func CmdJoin(c *cli.Context) {
-	// Write your code here
+	conf, err := config.LoadConf(c.GlobalString("config"))
+	if err != nil {
+		fmt.Printf("Unexpected error: %v", err)
+		os.Exit(1)
+	}
+	a10, err := client.NewClient(conf.A10)
+	if err != nil {
+		fmt.Printf("Unexpected error: %v", err)
+		os.Exit(1)
+	}
+	err = a10.Auth()
+	if err != nil {
+		fmt.Printf("Unexpected error: %v", err)
+		os.Exit(1)
+	}
+	defer a10.Close()
 
+	s, err := a10.ServerSearch(conf.Server.Host)
+	if err != nil {
+		fmt.Printf("Unexpected error: %v", err)
+		os.Exit(1)
+	}
+	if s != nil {
+		fmt.Printf("Server %v is already exist.", conf.Server.Host)
+	} else {
+		fmt.Printf("Create %v.", conf.Server.Host)
+		err = a10.ServerCreate(&conf.Server)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	for _, v := range conf.ServiceGroups {
+		sg, err := a10.ServiceGroupSearch(v.Name)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+			os.Exit(1)
+		}
+		if sg == nil {
+			fmt.Printf("ServiceGroup %v is not found.", v.Name)
+			os.Exit(1)
+		}
+		m := a10.SGMemberSearch(sg, v.Member.Server)
+		if m != nil {
+			fmt.Printf("Server %v is already exist.", v.Member.Server)
+		} else {
+			err = a10.ServiceGroupMemberCreate(&v)
+			if err != nil {
+				fmt.Printf("Unexpected error: %v", err)
+			}
+		}
+	}
 }
